ef9650dd7d0ae0577850222c0f169abfde1ddbc0
morghull <morghull@gmail.com>
2021-12-10 16:14:19 +0200

    finalize L78

--- components/NavMenu/index.js
+++ components/NavMenu/index.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect, useContext } from 'react';
+import React, { useReducer, useEffect, useContext, useRef } from 'react';
 import cx from 'classnames';
 import { NavLink } from 'react-router-dom';
 import CloseIcon from '@mui/icons-material/Close';
@@ -13,8 +13,18 @@ const NavMenu = () => {
   const classNames = cx(styles.container, {
     [styles.open]: isMenuOpen,
   });
+  const navRef = useRef(null); //пустая переменная - коробка
+  useEffect(() => {
+    const handlerClick = ({ target }) => {
+      if (isMenuOpen && !navRef.current.contains(target)) {
+        closeMenu();
+      }
+    };
+    window.addEventListener('click', handlerClick);
+    return () => window.removeEventListener('click', handlerClick);
+  }, [isMenuOpen]);
   return (
-    <nav className={classNames}>
+    <nav className={classNames} ref={navRef}> {/*в коробку помещаем эдемент*/}
       <CloseIcon className={styles.closeBtn} onClick={closeMenu} />
       <ul className={styles.list}>
         <li>
--- context/appReducer.js
+++ context/appReducer.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import CONSTANTS from '/constants';
 const { ACTIONS } = CONSTANTS;
 
